b702950dbfb59301e8d22d1099f4be4b2204004d
Adam Tuttle <adamtuttlecodes@gmail.com>
2017-12-29 16:54:59 -0500

    more tests including failing test for null

--- whitelist.test.js
+++ whitelist.test.js
@@ -1,8 +1,44 @@
 const whitelist = require('./whitelist');
 
-test('supports numeric scalar', () => {
+test('numeric scalar', () => {
 	expect(
-		whitelist({ numeric_int: 1, numeric_float: 1.01 }, ['numeric_int', 'numeric_float'])
+		whitelist({ numeric_int: 1, numeric_float: 1.01, other_int: 1, other_float: 1.01 }, [
+			'numeric_int',
+			'numeric_float'
+		])
 	).toEqual({ numeric_int: 1, numeric_float: 1.01 });
 });
 
+test('string scalar', () => {
+	expect(whitelist({ string: 'foo bar', other_string: 'fubar' }, ['string'])).toEqual({
+		string: 'foo bar'
+	});
+});
+
+test('boolean scalar', () => {
+	expect(whitelist({ bool: false, also_bool: true }, ['bool'])).toEqual({ bool: false });
+});
+
+test('null scalar', () => {
+	expect(whitelist({ nuallable: null, other: true }, ['nullable'])).toEqual({ nullable: null });
+});
+
+test('array of scalars', () => {
+	expect(whitelist({ arr: [true, 10, 'hello world'], other_arr: [1] }, ['arr'])).toEqual({
+		arr: [true, 10, 'hello world']
+	});
+});
+
+test('nested objects with deep key comparison', () => {
+	expect(
+		whitelist({ obj: { child: true, child_arr: [1], banned: 7 } }, ['obj', 'child', 'child_arr'])
+	).toEqual({
+		obj: { child: true, child_arr: [1] }
+	});
+});
+
+test('nested objects with shallow key comparison', () => {
+	expect(whitelist({ obj: { child: true }, other: false }, ['obj'], true)).toEqual({
+		obj: { child: true }
+	});
+});
